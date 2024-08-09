79113a03a825651ef8ccdb924acd357f5fa549a5
yufei20 <jyoti138maurya@gmail.com>
2024-08-10 01:07:10 +0530

    this is a modificatio about profiles done by shreya

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, MedicalRecord
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_picture')
+
+class MedicalRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'problem')
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(MedicalRecord, MedicalRecordAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    medical_history = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+    def save(self, *args, **kwargs):
+        # Prevent admin users from having a profile
+        if not self.user.is_superuser:
+            super().save(*args, **kwargs)
+
+class MedicalRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    problem = models.CharField(max_length=255)
+    document = models.FileField(upload_to='medical_documents/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
+
